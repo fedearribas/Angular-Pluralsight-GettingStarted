b734a501c8076510d990dada4d3e647c5d1aa4be
farribas <farribas@emmsa.net>
2023-01-18 01:34:43 -0300

    added shared module

--- src/app/products/product.module.ts
+++ src/app/products/product.module.ts
@@ -1,23 +1,18 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { ProductDetailComponent } from './product-detail.component';
 import { ProductListComponent } from './product-list.component';
-import { StarComponent } from '../shared/star.component';
 import { ConvertToSpacesPipe } from '../shared/convert-to-spaces.pipe';
-import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { ProductDetailGuard } from './product-detail.guard';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [
     ProductListComponent,
-    StarComponent,
     ConvertToSpacesPipe,
     ProductDetailComponent
   ],
   imports: [
-    CommonModule,
-    FormsModule,
     RouterModule.forChild([
       { path: 'products', component: ProductListComponent },
       { 
@@ -25,7 +20,8 @@ import { ProductDetailGuard } from './product-detail.guard';
         canActivate: [ProductDetailGuard],
         component: ProductDetailComponent
        }
-    ])
+    ]),
+    SharedModule
   ]
 })
 export class ProductModule { }
